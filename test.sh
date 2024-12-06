04053e072d5c2e882d207fb52e9ca740818d15af
arlecchino <github@tower-net.de>
2024-12-06 19:46:13 +0100

    test.sh pass on +version to cargo +... test

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ export RUST_BACKTRACE=1
 export RUSTFLAGS="-D dead_code -D unused-variables -D unused"
 
 for feature in "" tls tls-aws-lc-rs json charset cookies socks-proxy "tls native-certs" "tls-aws-lc-rs native-certs" native-tls gzip brotli http-interop http-crate; do
-  if ! cargo test --no-default-features --features "testdeps ${feature}" ; then
+  if ! cargo "$@" test --no-default-features --features "testdeps ${feature}" ; then
     echo Command failed: cargo test --no-default-features --features \"testdeps ${feature}\"
     exit 1
   fi
